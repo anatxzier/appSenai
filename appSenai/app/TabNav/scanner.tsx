92871c37493682786171b5e047175d8f4e87c836
anatxzier <anatxzier@gmail.com>
2024-04-25 13:52:05 -0300

    up scanner

--- appSenai/app/TabNav/scanner.tsx
+++ appSenai/app/TabNav/scanner.tsx
@@ -1,15 +1,44 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, SafeAreaView } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, SafeAreaView, Appearance } from 'react-native';
 import { Camera } from 'expo-camera';
+import useColor from '../../Temas/Temas';
 
 export default function Scanner() {
   const camRef = useRef(null);
+  const color = useColor(); 
   const [hasPermission, setHasPermission] = useState(false);
-
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: color.bgPrimary
+    },
+    camera: {
+      width: 450,
+      height: 450,
+    },
+    buttonContainer: {
+      position: 'absolute',
+      bottom: 20,
+      left: 20,
+    },
+    button: {
+      padding: 20,
+      backgroundColor: color.bgSecundary,
+      borderRadius: 10,
+    },
+    text: {
+      fontSize: 20,
+      color: color.bgPrimary, 
+    },
+  });
+  
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(String(status) === 'granted');
     })();
   }, []);
 
@@ -27,29 +56,3 @@ export default function Scanner() {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  camera: {
-    width: 450,
-    height: 450,
-  },
-  buttonContainer: {
-    position: 'absolute',
-    bottom: 20,
-    left: 20,
-  },
-  button: {
-    padding: 20,
-    backgroundColor: '#ffffff',
-    borderRadius: 10,
-  },
-  text: {
-    fontSize: 20,
-    color: '#000000',
-  },
-});
