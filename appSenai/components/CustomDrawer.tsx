6a8ac6f1adcc66310b1cced5656dbf4303fde059
anatxzier <anatxzier@gmail.com>
2024-04-25 13:05:19 -0300

    customdraewr

--- appSenai/components/CustomDrawer.tsx
+++ appSenai/components/CustomDrawer.tsx
@@ -1,13 +1,19 @@
-import { View, Text } from "react-native"
+import { DrawerItemList } from "@react-navigation/drawer";
+import { View, Text, Image } from "react-native";
+import useColor from "../Temas/Temas";
 
-export default function CustomDrawer(props: any){
+export default function CustomDrawer(props:any){
+    const color = useColor()
     return(
-        <View style={{marginTop: 60}}>
-            
-
-
-
-
+        <View style={{paddingTop: 60, backgroundColor: color.bgPrimary, height: '100%' }}>
+            <View style={{padding:20, display: 'flex', flexDirection:'row', borderBottomWidth: 1, borderBottomColor:'#D9D9D9'}}>
+                <Image source={require('@assets/Perfil.png')}/>
+                <View style={{display:'flex', justifyContent:'center', padding: 20}}>
+                    <Text style={{fontWeight: "bold", fontSize: 16, color: color.textcolorPrimary}}>Flavio Josefo</Text>
+                    <Text style={{color: color.textcolorSecundary}}>flavio@gmail.com</Text>
+                </View> 
+            </View>
+            <DrawerItemList {...props}/>
         </View>
     )
 }
