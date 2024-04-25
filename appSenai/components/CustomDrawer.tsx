a9d2892e43984eaff435d8ab4afe7df547ed1f00
anatxzier <anatxzier@gmail.com>
2024-04-25 13:09:39 -0300

    drawer

--- appSenai/components/CustomDrawer.tsx
+++ appSenai/components/CustomDrawer.tsx
@@ -9,8 +9,8 @@ export default function CustomDrawer(props:any){
             <View style={{padding:20, display: 'flex', flexDirection:'row', borderBottomWidth: 1, borderBottomColor:'#D9D9D9'}}>
                 <Image source={require('@assets/Perfil.png')}/>
                 <View style={{display:'flex', justifyContent:'center', padding: 20}}>
-                    <Text style={{fontWeight: "bold", fontSize: 16, color: color.textcolorPrimary}}>Flavio Josefo</Text>
-                    <Text style={{color: color.textcolorSecundary}}>flavio@gmail.com</Text>
+                    <Text style={{fontWeight: "bold", fontSize: 16, color: color.textcolorPrimary}}>Username</Text>
+                    <Text style={{color: color.textcolorSecundary}}>username@gmail.com</Text>
                 </View> 
             </View>
             <DrawerItemList {...props}/>
